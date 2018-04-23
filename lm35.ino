c13a11a5b75a2e36dde6e39540e70ee832ffcf8a
Somit_Sinha <somitsinha108@gmail.com>
2018-04-23 12:48:20 +0530

    Add files via upload

---
+++ lm35.ino
@@ -0,0 +1,28 @@
+#include<LiquidCrystal.h>
+const int rs = 2, en = 4, d4 = 11 , d5 = 10, d6 = 9, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); 
+const int sensor=A0; // Assigning analog pin A1 to variable 'sensor'
+float tempc;  //variable to store temperature in degree Celsius
+float tempf;  //variable to store temperature in Fahreinheit 
+float vout;  //temporary variable to hold sensor reading
+void setup()
+{
+pinMode(sensor,INPUT); // Configuring pin A1 as input
+Serial.begin(9600);
+lcd.begin(16,2);  
+  delay(500);
+}
+void loop() 
+{
+vout=analogRead(sensor);
+vout=(vout*500)/1023;
+tempc=vout; // Storing value in Degree Celsius
+tempf=(vout*1.8)+32; // Converting to Fahrenheit 
+lcd.setCursor(0,0);
+lcd.print("DegreeC= ");
+lcd.print(tempc);
+lcd.setCursor(0,1);
+lcd.print("Fahrenheit=");
+lcd.print(tempf);
+delay(1000); //Delay of 1 second for ease of viewing in serial monitor
+}
